a34743ebb807e11e2ec6f9cb62532233d71bb53b
akyo8 <vla.genya@mail.ru>
2021-04-23 01:40:11 -0400

    update WCLOAK interface

--- contracts/token/WCLOAK.sol
+++ contracts/token/WCLOAK.sol
@@ -2,16 +2,15 @@ pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts-upgradeable/presets/ERC20PresetMinterPauserUpgradeable.sol";
 
-contract WNAV is ERC20PresetMinterPauserUpgradeable {
-
+contract WCLOAK is ERC20PresetMinterPauserUpgradeable {
     address private _owner;
     address private _feeAddress;
     uint256 private _withdrawalFee;
 
     function initialize(string memory name, string memory symbol)
         public
-        initializer
         override
+        initializer
     {
         __ERC20PresetMinterPauser_init(name, symbol);
 
@@ -28,25 +27,35 @@ contract WNAV is ERC20PresetMinterPauserUpgradeable {
         emit BurnedWithNote(msg.sender, _value, _note);
     }
 
-    mapping (string => bool) mints;
+    mapping(string => bool) mints;
 
     event MintedWithNote(address indexed a, uint256 v, string n, uint256 f);
 
-    function mint(address _to, uint256 _amount, string memory _note, uint256 _fee) public {
-        require(hasRole(MINTER_ROLE, _msgSender()), "ERROR: must have minter role to mint");
+    function mint(
+        address _to,
+        uint256 _amount,
+        string memory _note,
+        uint256 _fee
+    ) public {
+        require(
+            hasRole(MINTER_ROLE, _msgSender()),
+            "ERROR: must have minter role to mint"
+        );
         require(!existsMint(_note));
         require(isRegistered(_to));
         super._mint(_to, _amount);
-        if (_fee != 0x0)
-        {
-          super._mint(_feeAddress, _fee);
+        if (_fee != 0x0) {
+            super._mint(_feeAddress, _fee);
         }
         mints[_note] = true;
         emit MintedWithNote(_to, _amount, _note, _fee);
     }
 
     function setFeeAddress(address _to) public {
-        require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "ERROR: must have admin role");
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
+            "ERROR: must have admin role"
+        );
         _feeAddress = _to;
     }
 
@@ -55,7 +64,10 @@ contract WNAV is ERC20PresetMinterPauserUpgradeable {
     }
 
     function setMinFee(uint256 _fee) public {
-        require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "ERROR: must have admin role");
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
+            "ERROR: must have admin role"
+        );
         _withdrawalFee = _fee;
     }
 
@@ -67,18 +79,17 @@ contract WNAV is ERC20PresetMinterPauserUpgradeable {
         return mints[_note] == true;
     }
 
-    mapping (address => bool) navAddresses;
+    mapping(address => bool) cloakAddresses;
 
     event Registered(address indexed a);
 
     function register() public {
         require(!isRegistered(msg.sender));
-        navAddresses[msg.sender] = true;
+        cloakAddresses[msg.sender] = true;
         emit Registered(msg.sender);
     }
 
     function isRegistered(address _native) public view returns (bool) {
-        return navAddresses[_native] == true;
+        return cloakAddresses[_native] == true;
     }
 }
-
